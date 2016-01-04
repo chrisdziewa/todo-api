b9f369cd4492825a2c0c0d945c1f63d7a2ea2dc5
Chris Dziewa <chris.dziewa@gmail.com>
2016-01-04 11:45:23 -0600

    Convert CRUD requests to use current user todos only

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ app.get('/', function(req, res) {
 // GET /todos
 app.get('/todos', middleware.requireAuthentication, function(req, res) {
   var query = req.query;
-  var where = {};
+  var where = {
+    userId: req.user.get('id')
+  };
 
   if (query.hasOwnProperty('completed')) {
     if (query.completed === 'true') {
@@ -44,7 +46,12 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
   var id = parseInt(req.params.id, 10);
 
-  db.todo.findById(id).then(function(todo) {
+  db.todo.findOne({
+    where: {
+      id: id,
+      userId: req.user.get('id')
+    }
+  }).then(function(todo) {
     if (todo) {
       res.json(todo);
     } else {
@@ -82,7 +89,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
   
   db.todo.destroy({
     where: {
-      id: id
+      id: id,
+      userId: req.user.get('id')
     }
   }).then(function(rowsDeleted) {
     if (rowsDeleted === 0) {
@@ -120,7 +128,12 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 
   var todoItem;
 
-  db.todo.findById(id).then(function(todo) {
+  db.todo.findOne({
+    where: {
+      id: id,
+      userId: req.user.get('id')
+    }
+  }).then(function(todo) {
     if (todo) {
       return todo.update(attributes);
     } else {
