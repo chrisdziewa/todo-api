6b54e4dfee7fa4f113b1156ae660ed05bfb40b55
Chris Dziewa <chris.dziewa@gmail.com>
2016-01-04 12:41:56 -0600

    Add DELETE /users/login

--- server.js
+++ server.js
@@ -167,17 +167,29 @@ app.post('/users', function(req, res) {
 // POST /users/login
 app.post('/users/login', function(req, res) {
   var body = _.pick(req.body, 'email', 'password');
+  var userInstance;
 
   db.user.authenticate(body).then(function(user) {
     var token = user.generateToken('authentication');
-    if (token) {
-      res.header('Auth', token).json(user.toPublicJSON());
-    } else {
-      return res.status(401).send('Authentication failed');
-    }
-
-  }, function(e) {
+    userInstance = user;
+    return db.token.create({
+      token: token
+    });
+  }).then(function(tokenInstance) {
+    res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+  }).catch(function(e) {
     res.status(401).send('Authentication failed');
+  });
+});
+
+// DELETE /users/login
+app.delete('/users/login', middleware.requireAuthentication, function(req, res) {
+  req.token.destroy().then(function() {
+    res.json({
+      success: 'User logged out'
+    });
+  }).catch(function() {
+    res.status(500).send('Server error');
   });
 });
 
